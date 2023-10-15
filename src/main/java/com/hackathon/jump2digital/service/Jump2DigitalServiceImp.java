86703cd8f7f7a693a573a5c29dbba3d2ea818e42
Pedro-Tejero-Diez <pedrete3000@hotmail.com>
2023-10-15 06:08:59 +0200

    update home 15 oct 2023

--- src/main/java/com/hackathon/jump2digital/service/Jump2DigitalServiceImp.java
+++ src/main/java/com/hackathon/jump2digital/service/Jump2DigitalServiceImp.java
@@ -2,21 +2,29 @@ package com.hackathon.jump2digital.service;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.hackathon.jump2digital.configuration.JsonFileProperties;
 import com.hackathon.jump2digital.document.Skin;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 @Service
 public class Jump2DigitalServiceImp {
-    @Autowired
-    JsonFileProperties jsonprops;
+    /*  @Autowired
+     JsonFileProperties jsonprops;
     public List<Skin> readJsonFile() throws IOException {
+         ObjectMapper objectMapper = new ObjectMapper();
+         return objectMapper.readValue(new File(jsonprops.getFilePath()),
+                 new TypeReference<List<Skin>>() {});
+     }
+ */
+ public List<Skin> readJsonFile() throws IOException {
+        Resource resource = new ClassPathResource("skins.json");
         ObjectMapper objectMapper = new ObjectMapper();
-        return objectMapper.readValue(new File(jsonprops.getFilePath()),
+        InputStream inputStream = resource.getInputStream();
+        return objectMapper.readValue(inputStream,
                 new TypeReference<List<Skin>>() {});
     }
 }
