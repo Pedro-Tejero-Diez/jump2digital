ebf6f229eb1ee8d9c602b2699435a2cbd562e302
Pedro-Tejero-Diez <pedrete3000@hotmail.com>
2023-10-24 06:18:50 +0200

    update home 24 oct 2023

--- src/main/java/com/hackathon/jump2digital/controller/PlayerSkinController.java
+++ src/main/java/com/hackathon/jump2digital/controller/PlayerSkinController.java
@@ -1,6 +1,8 @@
 package com.hackathon.jump2digital.controller;
 
+import com.hackathon.jump2digital.document.Player;
 import com.hackathon.jump2digital.document.PlayerSkin;
+import com.hackathon.jump2digital.document.Skin;
 import com.hackathon.jump2digital.service.PlayerServiceImp;
 import com.hackathon.jump2digital.service.PlayerSkinServiceImp;
 
@@ -9,10 +11,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 @Controller
 @RequestMapping("/Skins")
 public class PlayerSkinController {
@@ -46,4 +47,13 @@ public class PlayerSkinController {
                                      Model model) {
             return "playerskin";
         }
+
+    @PostMapping("/skins/buy")
+    public String butSkinPlayer(@ModelAttribute("player") Player player,
+                                @ModelAttribute("skin") Skin skin,
+                                RedirectAttributes ra) {
+        playerskinservice.buySkinById(player, skin);
+        ra.addAttribute("player_id", player.getPlayer_id());
+        return "redirect:/Skins/{player_id}/myskins";
+    }
     }
--- src/main/java/com/hackathon/jump2digital/controller/SkinController.java
+++ src/main/java/com/hackathon/jump2digital/controller/SkinController.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.io.IOException;
 import java.util.List;
@@ -25,77 +26,76 @@ public class SkinController {
     PlayerServiceImp service;
     @Autowired
     PlayerSkinServiceImp playerskinservice;
-    
+
     //WORKING
     @GetMapping("/new")
     public String newPlayer(Model model) {
         model.addAttribute("player", new Player());
         return "new_player";
     }
+
     @GetMapping("/login")
-    public String login (@ModelAttribute Skin skin, Model model) {
+    public String login(@ModelAttribute Skin skin, Model model) {
         model.addAttribute("player", new Player());
         return "login";
     }
 
     @GetMapping("/lookforplayer")
-    public String lookforplayer (@ModelAttribute Player player, Model model) {
+    public String lookforplayer(@ModelAttribute Player player,
+                                @ModelAttribute Skin skin, RedirectAttributes ra,
+                                Model model) {
 
         if (service.findByName(player.getName()) != null) {
-            model.addAttribute("playerskin",
-                    playerskinservice.getAllPlayerSkinByPlayer(player.getPlayer_id()));
-            return "playerskins";
-        } else return "not_found";
+          ra.addAttribute("player", player);
+          ra.addAttribute("skin", skin);
+            return "redirect: /skins/buy";
+        } else return "new_player";
     }
 
     @PostMapping("/add")
     public String savePlayer(@ModelAttribute("player") Player player) {
         try {
-        	if (service.findPlayerById(player.getPlayer_id()).isEmpty()) {
-            service.savePlayer(player);
-            return "success";
-        	} else return "oldplayer";
+            if (service.findPlayerById(player.getPlayer_id()).isEmpty()) {
+                service.savePlayer(player);
+                return "redirect: skins/buy";
+            } else return "oldplayer";
 
         } catch (Exception e) {
             return "error";
         }
     }
+
     //WORKING
     @GetMapping("/available")
     public String listSkins(Model model) throws IOException {
-            List<Skin> skins = service.readJsonFile();
-            model.addAttribute("skins", skins);
-            return "skins";
+        List<Skin> skins = service.readJsonFile();
+        model.addAttribute("skins", skins);
+        model.addAttribute("player", new Player());
+        return "skins";
     }
-    @GetMapping("/skins/buy/")
-    public String butSkinPlayer (@ModelAttribute ("player") Player player,
-                                 @ModelAttribute("skin") Skin skin, Model model) {
-    playerskinservice.buySkinById(player, skin);
-    model.addAttribute("player_id", player.getPlayer_id());
-        return "redirect:/Skins/{player_id}/myskins";
-    }
+
     //WORKING
-	@GetMapping("/{player_id}/myskins")
-	public String playerSkins(@PathVariable(value = "player_id") 
-	String player_id, Model model) {
+    @GetMapping("/{player_id}/myskins")
+    public String playerSkins(@PathVariable(value = "player_id")
+                              String player_id, Model model) {
 
-		try {
-			Player player = service.getPlayerById(player_id);
-			List<PlayerSkin> playerskin =  playerskinservice
-            		.getAllPlayerSkinByPlayer(player_id);
-			if (player != null) {
-				playerDTO playerdto = DtoConverter.toPlayerDTO(player);
-				model.addAttribute("playerskin",playerskin);
-				
+        try {
+            Player player = service.getPlayerById(player_id);
+            List<PlayerSkin> playerskin = playerskinservice
+                    .getAllPlayerSkinByPlayer(player_id);
+            if (player != null) {
+                playerDTO playerdto = DtoConverter.toPlayerDTO(player);
+                model.addAttribute("playerskin", playerskin);
 
-				model.addAttribute("player", playerdto);
-				return "player";
-			} else
-				return "no_encontrado";
-		} catch (Exception e) {
-			return "error";
-		}
-	}
+
+                model.addAttribute("player", playerdto);
+                return "player";
+            } else
+                return "no_encontrado";
+        } catch (Exception e) {
+            return "error";
+        }
+    }
 }
 
 	
