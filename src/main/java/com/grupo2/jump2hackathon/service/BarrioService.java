f92dd569656dc60743d7bbc176aa424e8a5889b1
pedrete3000 <pedrete3000@hotmail.com>
2023-11-21 15:22:44 +0100

    update6 jobplace 21 nov 2023

--- src/main/java/com/grupo2/jump2hackathon/service/BarrioService.java
+++ src/main/java/com/grupo2/jump2hackathon/service/BarrioService.java
@@ -1,19 +1,33 @@
 package com.grupo2.jump2hackathon.service;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.grupo2.jump2hackathon.documents.Barrio;
-import com.grupo2.jump2hackathon.documents.Geometry;
-import com.grupo2.jump2hackathon.documents.Poligono;
 import com.grupo2.jump2hackathon.repository.BarrioRepository;
+import io.github.classgraph.Resource;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
-import java.util.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class BarrioService {
     private final BarrioRepository barrioRepository;
 
+    public List<Barrio> readJsonFile() throws IOException {
+        Resource resource = new ClassPathResource("/barrios.json");
+        ObjectMapper objectMapper = new ObjectMapper();
+        InputStream inputStream = resource.getInputStream();
+        return objectMapper.readValue(inputStream,
+                new TypeReference<List<Barrio>>() {
+                });
+    }
     public List<Barrio> getAllBarrios() {
         return barrioRepository.findAll();
     }
